d26a47b68577656a4bf6b59f8bf5b125da673cd1
nymph0410 <71680170+nymph0410@users.noreply.github.com>
2021-11-12 14:48:49 +0800

    期中作業完成

--- 2D_game/Assets/Scripts/Playercontroller.cs
+++ 2D_game/Assets/Scripts/Playercontroller.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class Playercontroller : MonoBehaviour
 {
-   
+    [Header("移動速度")]
+    public float speed = 1;
+    [Header("跳躍高度")]
+    public float jump = 200f;
+    [Header("Run")]
+    public string parRun = "跑步開關";
+    [Header("Jump")]
+    public string parJump = "跳躍觸發";
+    [Header("Hit")]
+    public string parDead = "死亡開關";
+
 }
